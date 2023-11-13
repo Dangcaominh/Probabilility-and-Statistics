6afe5d484e94e4904c16f8d597ea97422fe1314f
Dangcaominh <minh.dangrandomname@hcmut.edu.vn>
2023-11-13 16:50:48 +0700

    Nothing

--- main.r
+++ main.r
@@ -1,4 +1,8 @@
 data <- read.csv(".\\data.csv")
-data2 <- data.frame(data[1], data[2], data[3])
-View(data2)
+data2 <- data.frame(
+  data[1], data[2], data[3],
+  data[4], data[5], data[6],
+  data[7], data[8], data[9]
+)
+print(apply(is.na(data2), 2, which))
 print("Anh gymer lỏ")
