42c9887d359e15850db860b5f71389dc34cf8975
Dangcaominh <minh.dangrandomname@hcmut.edu.vn>
2023-11-20 15:17:05 +0700

    Updated full code (maybe not the final one)

--- main.r
+++ main.r
@@ -171,9 +171,9 @@ max <- function(datainput) {
   return(array)
 }
 
-#--------------------------------------------------------------------------
-#-----------------------------MAIN CODE------------------------------------
-#--------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-----------------------------MAIN CODE-----------------------------------------
+#-------------------------------------------------------------------------------
 
 options(scipen = 99)
 library(ggplot2)
@@ -183,70 +183,61 @@ data <- read.csv(".\\data.csv")
 
 #-------------------------------------------------------------------------------
 # Lọc dữ liệu
-data2 <- data.frame(
+filtered_data <- data.frame(
   data[1], data[2], data[3],
   data[4], data[5], data[6],
-  data[7], data[8], data[9],
+  data[7], data[8], data[9]
 )
-View(data2)
-
-
+View(filtered_data)
 
 #-------------------------------------------------------------------------------
-# Lọc dữ liệu lần 2
-data2 <- data.frame(
-  data[1], data[2], data[3],
-  data[4], data[5], data[6],
-  data[7], data[8], data[9],
-)
-View(data2)
+# Kiểm tra dữ liệu bị khuyết
+print(apply(is.na(filtered_data), 2, which))
 
-data3 <- replace_value(data2, 0, 0.0000001)
-View(data3)
-data4 <- log_conversion(data3)
-View(data4)
-col.names <- get_col_name(data2)
-mean <- mean(data4)
-median <- median(data4)
-min <- min(data4)
-max <- max(data4)
-sd <- standard_derivation(data4)
+#-------------------------------------------------------------------------------
+# Chuyển đổi biến
+converted_data <- replace_value(
+  filtered_data,
+  c("grid", "honeycomb", "abs", "pla"),
+  c(0, 1, 0, 1)
+)
+
+View(converted_data)
+
+#-------------------------------------------------------------------------------
+# Tính các giá trị thống kê mẫu
+col.names <- get_col_name(converted_data)
+mean <- mean(converted_data)
+median <- median(converted_data)
+min <- min(converted_data)
+max <- max(converted_data)
+sd <- standard_derivation(converted_data)
 table <- data.frame(col.names, mean, median, min, max, sd)
 View(table)
 
-dev.new()
-# Histogram của layer height
-hist(data3[, "wall_thickness"],
-  xlab = "wall_thickness",
-  main = "Histogram of wall_thickness"
-)
-dev.new()
-hist(data4[, "wall_thickness"],
-  xlab = "log(wall_thickness)",
-  main = "Đồ thị Histogram of log(wall_thickness)"
-)
-dev.new()
-# Box plot của layer height và wall thickness
-boxplot(layer_height ~ wall_thickness,
-  main = "Boxplot of LH for each WT",
-  data = data3
-)
-dev.new()
-boxplot(layer_height ~ wall_thickness,
-  main = "Boxplot of log(LH) for each log(WT)",
-  data = data4
-)
+#-------------------------------------------------------------------------------
+# Vẽ heatmap
 
+# Tạo dataframe cho heatmap
 heatmap_data <- replace_value(
   data,
   c("grid", "honeycomb", "abs", "pla"),
   c(0, 1, 0, 1)
 )
-# Define a custom color palette
+
+# Tạo dải màu cho heatmap
 color_palette <- c("#00ffff", "#8877ff", "#ff00ff")
+
+# Tạo ma trận tương quan và làm tròn các hệ số đến hai số sau dấu phẩy
 corr_matr <- round(cor(heatmap_data), 2)
+
+# Reshape ma trận về dạng thích hợp để đưa vào heatmap
 melted_corr_matr <- melt(corr_matr)
+
+# Tạo cửa sổ graphic mới
 dev.new()
+
+# In heatmap
 print(
   ggplot(data = melted_corr_matr, aes(
     x = Var1,
@@ -258,26 +249,151 @@ print(
         Var2,
         label = value
       ),
-      color = "white", linewidth = 4
+      color = "white", size = 4
     ) +
     scale_fill_gradientn(colors = color_palette)
 )
 
-linear_regression_data <- heatmap_data
+#-------------------------------------------------------------------------------
+# Vẽ histogram
 
-# Fit a multiple linear regression model
+# Tạo cửa sổ graphic mới
+dev.new()
+
+# Vẽ histogram của layer height
+hist(converted_data[, "layer_height"],
+  xlab = "layer_height",
+  main = "Histogram của layer height"
+)
+
+# Tạo cửa sổ graphic mới
+dev.new()
+
+# Vẽ histogram của wall thickness
+hist(converted_data[, "wall_thickness"],
+  xlab = "wall_thickness",
+  main = "Histogram của wall thickness"
+)
+
+# Tạo cửa sổ graphic mới
+dev.new()
+
+# Vẽ histogram của infill density
+hist(converted_data[, "infill_density"],
+  xlab = "infill_density",
+  main = "Histogram của infill density"
+)
+
+# Tạo cửa sổ graphic mới
+dev.new()
+
+# Vẽ histogram của material
+hist(converted_data[, "material"],
+  xlab = "material",
+  main = "Histogram của material"
+)
+
+#-------------------------------------------------------------------------------
+# Tạo boxplot
+
+# Tạo dataframe cho boxplot
+boxplot_data <- replace_value(
+  data,
+  c("grid", "honeycomb", "abs", "pla"),
+  c(0, 1, 0, 1)
+)
+
+# Tạo cửa sổ graphic mới
+dev.new()
+
+# Boxplot cho layer height và roughness
+boxplot(layer_height ~ roughness,
+  data = boxplot_data,
+  main = "Boxplot cho layer height và roughness"
+)
+
+# Tạo cửa sổ graphic mới
+dev.new()
+
+# Boxplot cho layer height và elongation
+boxplot(layer_height ~ elongation,
+  data = boxplot_data,
+  main = "Boxplot cho layer height và elongation"
+)
+
+# Tạo cửa sổ graphic mới
+dev.new()
+
+# Boxplot cho wall thickness và tension strength
+boxplot(wall_thickness ~ tension_strenght,
+  data = boxplot_data,
+  main = "Boxplot cho wall thickness và tension strength"
+)
+
+#-------------------------------------------------------------------------------
+# Hồi quy tuyến tính đa biến
+
+# Tạo dataframe cho hồi quy tuyến tính
+linear_regression_data <- replace_value(
+  data,
+  c("grid", "honeycomb", "abs", "pla"),
+  c(0, 1, 0, 1)
+)
+
+# Hồi quy tuyến tính đa biến cho biến roughness và 8 biến input
 model <- lm(formula =
-    linear_regression_data[, 10] ~
-    (linear_regression_data[, 1] +
-     linear_regression_data[, 2] +
-     linear_regression_data[, 3] +
-     linear_regression_data[, 4] +
-     linear_regression_data[, 5] +
-     linear_regression_data[, 6] +
-     linear_regression_data[, 7] +
-     linear_regression_data[, 8]) ,
+    roughness ~
+    (layer_height +
+     wall_thickness +
+     infill_density +
+     infill_pattern +
+     nozzle_temperature +
+     bed_temperature +
+     print_speed +
+     material),
   data = linear_regression_data
 )
 
-# Print the summary of the model
+# In ra kết quả
+print("Kết quả hồi quy tuyến tính đa biến cho biến roughness và 8 biến input")
 print(summary(model))
+
+# Hồi quy tuyến tính đa biến cho biến tension strength và 8 biến input
+model <- lm(formula =
+    tension_strenght ~
+    (layer_height +
+     wall_thickness +
+     infill_density +
+     infill_pattern +
+     nozzle_temperature +
+     bed_temperature +
+     print_speed +
+     material),
+  data = linear_regression_data
+)
+
+# In ra kết quả
+print("Kết quả hồi quy tuyến tính đa biến cho biến tension strength
+và 8 biến input")
+print(summary(model))
+
+# Hôi quy tuyến tính đa biến cho biến elongation và 8 biến input
+model <- lm(formula =
+    elongation ~
+    (layer_height +
+     wall_thickness +
+     infill_density +
+     infill_pattern +
+     nozzle_temperature +
+     bed_temperature +
+     print_speed +
+     material),
+  data = linear_regression_data
+)
+
+# In ra kết quả
+print("Kết quả hồi quy tuyến tính đa biến cho biến elongation và 8 biến input")
+print(summary(model))
+
+path <- R.home()
+print(path)
